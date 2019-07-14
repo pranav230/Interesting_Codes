31a5f254d08f7439ccca9eb023169ba30a9f30ba
pranav230 <37182018+pranav230@users.noreply.github.com>
2019-07-15 00:35:51 +0530

    removing vector element by value in c++

---
+++ Vectors.cpp
@@ -0,0 +1,8 @@
+/*
+Some Codes that are useful while Competitve Coding
+*/
+
+
+//1. Way to erase an element from a vector by value
+
+vector_name.erase(find(vector_name.begin(),vector_name.end(),element_value));
