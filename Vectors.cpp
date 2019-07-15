a8900403bcf2ed23493021e091e5e70d5364bee4
pranav230 <37182018+pranav230@users.noreply.github.com>
2019-07-16 00:10:52 +0530

    distance between iterators is updated

--- Vectors.cpp
+++ Vectors.cpp
@@ -7,5 +7,8 @@ Some Codes that are useful while Competitve Coding
 
 vector_name.erase(find(vector_name.begin(),vector_name.end(),element_value));
 
+//2. Way to find the difference between 2 iterators of same type
+
+int num = distance(iterator1,iterator2);
 
 
